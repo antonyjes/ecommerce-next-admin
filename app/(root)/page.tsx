527d830d5711c1918ddeb215a1378f17e0eb8a44
antonyjes <72809149@continental.edu.pe>
2023-07-03 20:33:03 -0500

    Auth with Clerk

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs"
+
+const Home = () => {
+  return(
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  )
+}
+
+export default Home;
