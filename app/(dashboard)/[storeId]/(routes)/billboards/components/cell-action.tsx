f0e645bbd242f4ac38eae94dca252578e3a7fd35
antonyjes <72809149@continental.edu.pe>
2023-07-15 21:12:04 -0500

    DataTables Shadcn

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { BillboardColumn } from "./columns";
+
+interface CellActionProps {
+    data: BillboardColumn;
+}
+
+export const CellAction: React.FC<CellActionProps> = ({data}) => {
+    return(
+        <div>Actions</div>
+    )
+};
